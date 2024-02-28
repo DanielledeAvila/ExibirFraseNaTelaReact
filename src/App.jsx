e1710d8c5124303629fd1dde1446aa72436bb6ea
Danielle de Avila <danivandinha1@gmail.com>
2024-02-27 21:07:36 -0300

    adicionei todos os json

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./componentes/Header/header.jsx"
+import "../src/App.css"
+
+
+function App(){
+  return(
+    <>
+    <h1>Exibindo a frase no global, no App.jsx</h1>
+    <Header/>
+    </>
+    
+  )
+}
+
+export default App
